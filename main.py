da487579ca6f0aba74ee63373e691d074c65a713
ntnkiev <ntn.kiev@gmail.com>
2024-03-29 16:31:11 +0200

    comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ URL = "https://quotes.toscrape.com"
 
 
 def page_scrape(url, authors, quotes) -> str | None:
+    # функція скрапінгу сторінки about автора
     def authors_scrape(authors_url, authors):
         response = requests.get(authors_url)
         soup = BeautifulSoup(response.text, 'lxml')
@@ -13,27 +14,29 @@ def page_scrape(url, authors, quotes) -> str | None:
         born_date = soup.find("span", class_="author-born-date").get_text().strip()
         born_location = soup.find("span", class_="author-born-location").get_text().strip()
         description = soup.find("div", class_="author-description").get_text().strip()
+        # заповнення списку авторів
         authors.append(
             {"fullname": fullname, "born_date": born_date, "born_location": born_location, "description": description})
 
+    # скрапінг сторінки цитат
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "lxml")
     page_quotes = soup.find_all('span', class_='text')
     page_authors = soup.find_all('small', class_='author')
     page_tags = soup.find_all('div', class_='tags')
-    page_about = soup.select("[href^='/author']")
+    page_about = soup.select("[href^='/author']")  # формування лінка сторінки автора
     for a in page_about:
         authors_url = URL + a['href']
-        authors_scrape(authors_url, authors)
+        authors_scrape(authors_url, authors)  # виклик функції скрапінгу сторінки about автора
 
-    for i in range(0, len(page_quotes)):
+    for i in range(0, len(page_quotes)):  # заповнення списку цитат
         tag_list = []
         tagsforquote = page_tags[i].find_all('a', class_='tag')
         for tagforquote in tagsforquote:
             tag_list.append(tagforquote.text)
         quotes.append({"tags": tag_list, "author": page_authors[i].text, "quote": page_quotes[i].text})
 
-    next_url = soup.find('li', class_='next')
+    next_url = soup.find('li', class_='next')  # пошук посилання на наступну сторінку
     if not next_url:
         return None
     next_url = URL + next_url.find('a')['href']
@@ -45,7 +48,7 @@ if __name__ == "__main__":
     authors = []
     quotes = []
     new_url = URL
-    while True:
+    while True:  # перебір всіх сторінок
         new_url = page_scrape(new_url, authors, quotes)
         if not new_url:
             break
